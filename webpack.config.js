9d87ead180c7b1eaa858e99f52f2db8ee13c4846
moschan <kaityo02@gmail.com>
2016-02-15 06:57:10 +0800

    update

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  entry: './index.js',
+
+  output: {
+    filename: 'index.js',
+    path: './dist'
+  },
+
+  module: {
+    loaders: [
+      { test: /\.css$/, loader: 'style!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]' },
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
+    ]
+  }
+}
