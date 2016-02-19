239cbd9195a762a3337b2a3f82a0d2717565666e
moschan <kaityo02@gmail.com>
2016-02-19 23:20:28 +0800

    update

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ import Gidget from './lib/Gidget.js'
 export default Gidget
 
 console.log('hello, inx')
-
+console.log('hello, inx')
+console.log('hello, inx')
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 module.exports = {
-  entry: './index.js',
+  entry: {
+    index: './index.js'
+  },
 
   output: {
-    filename: 'index.js',
-    path: './example/dist/'
+    path: './example/dist',
+    publicPath: '/dist/',
+    filename: '[name].js'
   },
 
   module: {
@@ -11,11 +14,10 @@ module.exports = {
       { test: /\.css$/, loader: 'style!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]' },
       { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
     ]
-  },
-  devServer: {
-    contentBase: './example',
-    port: 8080,
-    hot: true,
-    inline: true
   }
+  // devServer: {
+  //   contentBase: 'example/',
+  //   port: 8080,
+  //   hot: true
+  // }
 }
